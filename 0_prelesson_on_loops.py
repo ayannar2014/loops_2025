b15cffa58a44d5a56de2e5dcaab05acd51a4f5df
ayannar2014 <arodriguez021480@gmail.com>
2025-12-08 19:35:34 +0000

    prelesson_loops

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -1,2 +1,30 @@
-Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
-While loops  
+# Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
+# While loops  
+# for loops=execute a block of code a fixed number of times. 
+#u iterate over a range, string, sequence, etc. 
+# for x in range(1,11,3):
+#     print(x)
+
+# print("Happy New Year!")
+
+# credit_card = "1234-5678-9012-3456"
+# for x in credit_card:
+#     print(x)
+
+# for x in range (1,21): 
+#     if x==13:
+#         break #break will mat it stop at 13...continue will make it skip 13 but continue after
+#     else: 
+#         print(x) 
+
+#WILDLOOPS
+#execute code WHILE some conditions remain true
+
+# name = input("Enter your name:") 
+
+# while name == "": 
+#     print("You did not enter your name.")
+
+#     name = input("Enter your name:") 
+
+# print("Hello {name}") 
